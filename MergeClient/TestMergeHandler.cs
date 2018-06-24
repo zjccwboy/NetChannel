1aeccbaa1aa9915572653122d4a2e34aa1170623
zjccwboy <70380130@qq.com>
2018-06-24 19:22:45 +0800

    修改:改成标准库工程

--- MergeClient/TestMergeHandler.cs
+++ MergeClient/TestMergeHandler.cs
@@ -1,11 +1,7 @@
 ﻿using NetChannel;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace MergeClient
 {
