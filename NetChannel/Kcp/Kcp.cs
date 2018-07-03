a410c6fa9c1561aad3fefcea9a7d28ba8b19753a
sam <70380130@qq.com>
2018-07-03 18:24:50 +0800

    修改:KCP

--- NetChannel/Kcp/Kcp.cs
+++ NetChannel/Kcp/Kcp.cs
@@ -145,61 +145,7 @@ namespace NetChannel
         private static Int32 _itimediff(UInt32 later, UInt32 earlier)
         {
             return (Int32)(later - earlier);
-        }
-
-        // KCP Segment Definition
-        internal class Segment
-        {
-            internal UInt32 conv;
-            internal UInt32 cmd;
-            internal UInt32 frg;
-            internal UInt32 wnd;
-            internal UInt32 ts;
-            internal UInt32 sn;
-            internal UInt32 una;
-            internal UInt32 resendts;
-            internal UInt32 rto;
-            internal UInt32 fastack;
-            internal UInt32 xmit;
-            internal byte[] data;
-
-            internal void flush()
-            {
-                conv = 0;
-                cmd = 0;
-                frg = 0;
-                wnd = 0;
-                ts = 0;
-                sn = 0;
-                una = 0;
-                resendts = 0;
-                rto = 0;
-                fastack = 0;
-                xmit = 0;
-            }
-
-            internal Segment(int size)
-            {
-                this.data = new byte[size];
-            }
-
-            // encode a segment into buffer
-            internal int encode(byte[] ptr, int offset)
-            {
-                var offset_ = offset;
-
-                offset += ikcp_encode32u(ptr, offset, conv);
-                offset += ikcp_encode8u(ptr, offset, (byte)cmd);
-                offset += ikcp_encode8u(ptr, offset, (byte)frg);
-                offset += ikcp_encode16u(ptr, offset, (UInt16)wnd);
-                offset += ikcp_encode32u(ptr, offset, ts);
-                offset += ikcp_encode32u(ptr, offset, sn);
-                offset += ikcp_encode32u(ptr, offset, una);
-                offset += ikcp_encode32u(ptr, offset, (UInt32)data.Length);
-
-                return offset - offset_;
-            }
-        }
+        }      
 
         // kcp members.
         private readonly UInt32 conv;
---
+++ NetChannel/Kcp/Segment.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetChannel
+{
+    /// <summary>
+    /// KCP Segment Definition
+    /// KCP分片包定义
+    /// </summary>
+    internal class Segment
+    {
+        internal UInt32 conv;
+        internal UInt32 cmd;
+        internal UInt32 frg;
+        internal UInt32 wnd;
+        internal UInt32 ts;
+        internal UInt32 sn;
+        internal UInt32 una;
+        internal UInt32 resendts;
+        internal UInt32 rto;
+        internal UInt32 fastack;
+        internal UInt32 xmit;
+        internal byte[] data;
+
+        internal void flush()
+        {
+            conv = 0;
+            cmd = 0;
+            frg = 0;
+            wnd = 0;
+            ts = 0;
+            sn = 0;
+            una = 0;
+            resendts = 0;
+            rto = 0;
+            fastack = 0;
+            xmit = 0;
+        }
+
+        internal Segment(int size)
+        {
+            this.data = new byte[size];
+        }
+
+        // encode a segment into buffer
+        internal int encode(byte[] ptr, int offset)
+        {
+            var offset_ = offset;
+
+            offset += Kcp.ikcp_encode32u(ptr, offset, conv);
+            offset += Kcp.ikcp_encode8u(ptr, offset, (byte)cmd);
+            offset += Kcp.ikcp_encode8u(ptr, offset, (byte)frg);
+            offset += Kcp.ikcp_encode16u(ptr, offset, (UInt16)wnd);
+            offset += Kcp.ikcp_encode32u(ptr, offset, ts);
+            offset += Kcp.ikcp_encode32u(ptr, offset, sn);
+            offset += Kcp.ikcp_encode32u(ptr, offset, una);
+            offset += Kcp.ikcp_encode32u(ptr, offset, (UInt32)data.Length);
+            return offset - offset_;
+        }
+    }
+}
